d2fef9504fe8d4cba02322f6447e5ef94967dca0
guilh <guilh@Guilherme-PC>
2018-03-01 22:29:34 -0300

    criando o CadastroProfessor

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,29 @@
+package sistema.modelos;
+
+public class Professor {
+	
+	private int matricula;
+	private String nome;
+	private String materia;
+	
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getMateria() {
+		return materia;
+	}
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+	
+}
